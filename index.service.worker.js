dcfc0b851553297823594f9502afaa4ff1eb9b59
Peti <peter.hathazi99@gmail.com>
2026-07-13 13:42:59 +0200

    test_19

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1783942403|22402791533';
+const CACHE_VERSION = '1783942528|22528393326';
 /** @type {string} */
 const CACHE_PREFIX = 'VakKaland-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
